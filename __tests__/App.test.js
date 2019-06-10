f896246388d0e07ad71121d96fae748cf12767d5
Justyna Sowińska <just.sowinska@gmail.com>
2019-06-10 08:58:56 +0200

    add jest and enzyme

---
+++ __tests__/App.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from '../App';
+
+it('renders correctly', () => {
+  const component = shallow(
+    <App />
+  );
+
+  expect(component).toMatchSnapshot();
+});
---
+++ src/__tests__/TestComponent.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import { TestComponent } from '../TestComponent';
+
+it('renders correctly', () => {
+  const component = shallow(
+    <TestComponent />
+  );
+
+  expect(component).toMatchSnapshot();
+});
